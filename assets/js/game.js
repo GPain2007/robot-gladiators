d38d66939c731c60d35f5452a92adf6fc0558b3d
Govnor Payne <govnorpayne@gmail.com>
2022-02-16 13:37:04 -0600

    added fight functions and string elements

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("this window is working");
+//window.alert("this window is working");
+//this creates the fighting function
+
+function fight() {
+  window.alert("the fight has begun");
+}
+fight();
+var playerName = "tony the robot";
+
+window.prompt("What is your robot's name?");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+//what is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
